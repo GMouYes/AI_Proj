1c97019b1939f04ad41bf03bd7048fe3752294b9
Bowen <btang@wpi.edu>
2020-02-12 20:48:19 -0500

    add a star algo and board class

--- Queens/Board.py
+++ Queens/Board.py
@@ -15,8 +15,39 @@ def readBoard(fileName):
 
 class board(object):
 	"""docstring for board"""
-	def __init__(self, arg):
+	def __init__(self, state: list):
 		# sth to implement here
 		super(board, self).__init__()
-		self.arg = arg
-		
+		self.state = state
+
+	# @staticmethod
+	# def cost(b1, b2):
+	# 	# return cost
+
+	def heuristic(self):
+		# return heuristic
+
+
+	def get_neighbors(self):
+		# return all next states and  its cost;
+		neighbors = []
+		positions = []
+		n = len(self.state)
+		for i in range(n):
+			for j in range(n):
+				if self.state[i][j] != ',':
+					positions.append((j, self.state[i][j]))
+		for i in range(n):
+			for j in range(n):
+				if self.state[i][j] == ',':
+					new_list = self.state[:]
+					new_list[i][j], new_list[i][positions[i][0]] = new_list[i][positions[i][0]], new_list[i][j]
+					cost = abs(j - positions[i][0])*positions[i][1]**2
+					neighbors.append((board(new_list), cost))
+		return neighbors
+
+
+
+	def finished(self):
+		# return boolean
+		return self.heuristic() == 0
--- Queens/searchAlgo.py
+++ Queens/searchAlgo.py
@@ -1,8 +1,42 @@
-from Board inport *
+from queue import PriorityQueue
+from Board import *
+import time
 
-def A_Star():
+def A_Star(start: board):
 	# sth to implement here
-	pass
+	frontier = PriorityQueue()
+	frontier.put((0, start))
+	came_from = dict()
+	cost_so_far = dict()
+	came_from[start] = None
+	cost_so_far[start] = 0
+	cur_state = None
+
+	start_time = time.time
+
+	while not frontier.empty():
+		cur_state = frontier.get()[1]
+
+		if cur_state.finished():
+			break
+
+		for next_state in cur_state.neighbors():
+			new_cost = cost_so_far[cur_state] + next_state[1]
+			if next_state[0] not in cost_so_far or new_cost < cost_so_far[next_state[0]]:
+				cost_so_far[next_state] = new_cost
+				priority = new_cost + next_state[0].heuristic()
+				frontier.put((priority, next_state))
+				came_from[next_state[0]] = cur_state
+
+	end_time = time.time
+
+	path = []
+	while cur_state != start:
+		path.append(cur_state)
+		cur_state = came_from[cur_state]
+	path.append(start)
+	# please add the time elapsedtime to return statement
+	return path.reverse()
 
 def greedyHillClimb():
 	# sth to implement here
