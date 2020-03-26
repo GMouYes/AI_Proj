ae47769b4d6d26c1347a82c429e0bd1f8f9d690f
yinhao0424 <yinhao0424@gmail.com>
2020-03-26 10:58:27 -0400

    change result format

--- Proj2/algo.py
+++ Proj2/algo.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import time
 from scipy.stats import multivariate_normal
+import copy
 import matplotlib.pyplot as plt
 
 
@@ -75,18 +76,20 @@ def EMClustering(data, clusters):
 
     # change these constants for more tests
     random_start_times = 0
-    random_start_max = 4
+    random_start_max = 3
     random_ratio = .3
 
     diff = 0.0001
     logLikelihood = [1]
     result ={}
     start_time = time.time()
-
+    bestresult = None
     if clusters > 0:
+
         while random_start_times < random_start_max and time.time() - start_time < 10:
             mean, cov, weight = InitializeCluster(data, clusters, random_ratio)
             temp = 0
+
             while abs(logLikelihood[-1]- temp) > diff and time.time() - start_time < 10:  # you define your own input/output
                 temp = logLikelihood[-1]
                 # expectation
@@ -98,32 +101,40 @@ def EMClustering(data, clusters):
 
                 if log_sum > temp:
                     result['clusterCenters'] = [item for item in zip(mean,cov)]
-                    result['logLikelihood'] = logLikelihood[-1]
+                    result['logLikelihood'] = log_sum
+                    result['weight'] = weight
 
                 logLikelihood.append(log_sum)
 
+
             random_start_times += 1
-        # print(random_start_times+1)
+
+            if not bestresult or result['logLikelihood'] > bestresult['logLikelihood']:
+                bestresult = copy.deepcopy(result)
+
+        # print(random_start_times)
         # print(logLikelihood)
         #
         # plt.plot(logLikelihood[1:])
         # plt.xlabel('number of iterations')
         # plt.ylabel('Log-likelihood')
         # plt.show()
+        bestresult.update({'restart':random_start_times, 'time':time.time() - start_time,
+                           'logLikelihood_list':logLikelihood })
 
-        return result
+        return bestresult
 
     else:
         clusters = 1
-
         BIC = {}
         final_result = {}
-        while True and time.time() - start_time < 10:
+
+        while time.time() - start_time < 10:
+
             random_ratio =  .3
             temp = 0
-            mean, cov, weight = InitializeCluster(data, clusters, random_ratio)
-            result ={}
             logLikelihood = [1]
+            mean, cov, weight = InitializeCluster(data, clusters, random_ratio)
 
             while abs(logLikelihood[-1]- temp) > diff and time.time() - start_time < 10:  # you define your own input/output
                 temp = logLikelihood[-1]
@@ -137,20 +148,25 @@ def EMClustering(data, clusters):
                 if log_sum > temp:
                     result['clusterCenters'] = [item for item in zip(mean,cov)]
                     result['logLikelihood'] = logLikelihood[-1]
+                    result['weight'] = weight
                     BIC[clusters] = -2 * logLikelihood[-1] + np.log(len(data)) * clusters * 2
 
                 logLikelihood.append(log_sum)
-            final_result[clusters] = result
+
+            result['logLikelihood_list'] = logLikelihood
+            final_result[clusters] = copy.deepcopy(result)
             clusters += 1
 
         best_cluster = max(BIC, key=BIC.get)
-        return best_cluster,BIC[best_cluster],final_result[best_cluster]
 
-      # you have to return the required dict
-if __name__ == '__main__':
-    data = pd.read_csv('sample EM data.csv',header=None).values
-    # best_cluster, value,final = EMClustering(data, 0)
-    # print(best_cluster, value,final)
 
-    result = EMClustering(data, 2)
-    print(result)
+        output = {'BestNumberOfCluster':best_cluster, 'BIC':BIC[best_cluster],
+                  'clusterCenters':final_result[best_cluster]['clusterCenters'],
+                  'logLikelihood':final_result[best_cluster]['logLikelihood'],
+                  'weight': final_result[best_cluster]['weight'],
+                  'time': time.time() - start_time,
+                  'logLikelihood_list': final_result[best_cluster]['logLikelihood']
+                  }
+
+        return output
+    
