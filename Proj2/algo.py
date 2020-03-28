d4c1522d53aeafd22f09529c91a675dfb4c717ed
mgy920923 <mgy920923@gmail.com>
2020-03-28 04:29:50 -0400

    redesign the structure of algos

--- Proj2/algo.py
+++ Proj2/algo.py
@@ -21,13 +21,7 @@ def Maximization(responsibility, data):
     num_data, dim_data = data.shape
     num_cluster = responsibility.shape[0]
 
-    norm_res = responsibility / np.sum(responsibility, axis=1).reshape(-1, 1)  # same shape
-
-    # new_mean = np.zeros((num_cluster, dim_data))
-    # for j in range(num_cluster):
-    #   for n in range(num_data):
-    #       new_mean[j] += norm_res[j][n] * data[n,:]
-
+    norm_res = responsibility / np.sum(responsibility, axis=1).reshape(-1, 1)  # (#clusters, #data)
 
     new_mean = norm_res.dot(data)  # clusters, dim_data
 
@@ -45,6 +39,7 @@ def Maximization(responsibility, data):
 def InitializeCluster(data, clusters, random_ratio):
     # the way we initialize:
     # pick instances randomly and calculate parameters
+    # we could have run knn first but that takes time
     num_data, dim_data = data.shape
     num_rows = int(num_data * random_ratio)
 
@@ -63,110 +58,93 @@ def InitializeCluster(data, clusters, random_ratio):
 
 
 def Loglikelihood(data, mean, cov, weight):
-    # use sum of log-likelihood
-    # multivariate_normal.logpdf()
     likelihood = [multivariate_normal.pdf(data, *item) for item in zip(mean, cov)]
     log_sum = np.sum(np.log(np.sum(weight * np.array(likelihood).T, axis=1)))
 
     return log_sum
 
-# placeholder function
-def EMClustering(data, clusters):
+def timeDiff(startTime, period):
+    return (time.time() - startTime) < period
+
+def convergence(value1, value2, threshold):
+    return abs(value1 - value2) < diff
+
+def updateResult(mean, cov, weight, log_sum, restart, start_time, clusters, num_data):
+    # notice: this is the "time" to find the best solution, not total time
+    # if you wish to include total time, please add another dict key
+    # the same thing applies on entrance "restart"
+    result = {
+        "clusterCenters": list(zip(mean,cov)),
+        "logLikelihood": log_sum,
+        "weight": weight,
+        "restart": restart,
+        "time": time.time() - start_time, 
+        "clusters": clusters,
+        "BIC": -2 * log_sum + np.log(num_data) * clusters * 2,
+    }
+    return result
+
+def Clustering(data, clusters, maxTime, random_start_times, random_ratio, start_time, diff):
     num_data, dim_data = data.shape
 
+    for epoch in range(random_start_times):
+
+        mean, cov, weight = InitializeCluster(data, clusters, random_ratio)
+        log_sum = Loglikelihood(data, mean, cov, weight)
+        result = updateResult(mean, cov, weight, log_sum, epoch+1, \
+            start_time, clusters, num_data)
+
+        old_log_sum = log_sum
+        while True:
+            # expectation
+            responsibility = Expectation(data, mean, cov, weight)
+            # maximization
+            mean, cov, weight = Maximization(responsibility, data)
+            # evaluation
+            log_sum = Loglikelihood(data, mean, cov, weight)
+            # store better global result
+            if log_sum > result["logLikelihood"]:
+                result = updateResult(mean, cov, weight, log_sum, epoch+1, \
+                    start_time, clusters, num_data)
+            # begin next random start if the current converged
+            if convergence(old_log_sum, log_sum, diff):
+                break
+            # return global best if time limit is reached
+            if not timeDiff(start_time, maxTime):
+                return result
+            # update old log sum 
+            old_log_sum = log_sum
+
+    return result
+
+
+def EMClustering(data, clusters):
+
     # change these constants for more tests
-    random_start_times = 0
-    random_start_max = 3
+    random_start_times = 3
     random_ratio = .3
+    maxPeriod = 10 # algo should run no longer than 10 secs
+    diff = 0.0001 # convergence threshold
 
-    diff = 0.0001
-    logLikelihood = [1]
-    result ={}
-    start_time = time.time()
-    bestresult = None
     if clusters > 0:
-
-        while random_start_times < random_start_max and time.time() - start_time < 10:
-            mean, cov, weight = InitializeCluster(data, clusters, random_ratio)
-            temp = 0
-
-            while abs(logLikelihood[-1]- temp) > diff and time.time() - start_time < 10:  # you define your own input/output
-                temp = logLikelihood[-1]
-                # expectation
-                responsibility = Expectation(data, mean, cov, weight)
-                # maximization
-                mean, cov, weight = Maximization(responsibility, data)
-                # evaluation
-                log_sum = Loglikelihood(data, mean, cov, weight)
-
-                if log_sum > temp:
-                    result['clusterCenters'] = [item for item in zip(mean,cov)]
-                    result['logLikelihood'] = log_sum
-                    result['weight'] = weight
-
-                logLikelihood.append(log_sum)
-
-
-            random_start_times += 1
-
-            if not bestresult or result['logLikelihood'] > bestresult['logLikelihood']:
-                bestresult = copy.deepcopy(result)
-
-        # print(random_start_times)
-        # print(logLikelihood)
-        #
-        # plt.plot(logLikelihood[1:])
-        # plt.xlabel('number of iterations')
-        # plt.ylabel('Log-likelihood')
-        # plt.show()
-        bestresult.update({'restart':random_start_times, 'time':time.time() - start_time,
-                           'logLikelihood_list':logLikelihood })
-
-        return bestresult
+        start_time = time.time()
+        result = Clustering(data, clusters, maxPeriod, random_start_times, random_ratio, start_time, diff)
+        return result
 
     else:
-        clusters = 1
-        BIC = {}
-        final_result = {}
+        start_time = time.time()
+        period = maxPeriod
+        temp_best = float("inf")
+        for candidate_cluster in range(1, data.shape[0]):
+            temp_result = Clustering(data, candidate_cluster, period, random_start_times, random_ratio, start_time, diff)
+            period -= result["time"]
+            # store better scores
+            if temp_result["BIC"] < temp_best:
+                result = copy.deepcopy(temp_result)
+                temp_best = temp_result["BIC"]
+            # return global best if time limit is reached
+            if not timeDiff(start_time, period):
+                return result
 
-        while time.time() - start_time < 10:
-
-            random_ratio =  .3
-            temp = 0
-            logLikelihood = [1]
-            mean, cov, weight = InitializeCluster(data, clusters, random_ratio)
-
-            while abs(logLikelihood[-1]- temp) > diff and time.time() - start_time < 10:  # you define your own input/output
-                temp = logLikelihood[-1]
-                # expectation
-                responsibility = Expectation(data, mean, cov, weight)
-                # maximization
-                mean, cov, weight = Maximization(responsibility, data)
-                # evaluation
-                log_sum = Loglikelihood(data, mean, cov, weight)
-
-                if log_sum > temp:
-                    result['clusterCenters'] = [item for item in zip(mean,cov)]
-                    result['logLikelihood'] = logLikelihood[-1]
-                    result['weight'] = weight
-                    BIC[clusters] = -2 * logLikelihood[-1] + np.log(len(data)) * clusters * 2
-
-                logLikelihood.append(log_sum)
-
-            result['logLikelihood_list'] = logLikelihood
-            final_result[clusters] = copy.deepcopy(result)
-            clusters += 1
-
-        best_cluster = max(BIC, key=BIC.get)
-
-
-        output = {'BestNumberOfCluster':best_cluster, 'BIC':BIC[best_cluster],
-                  'clusterCenters':final_result[best_cluster]['clusterCenters'],
-                  'logLikelihood':final_result[best_cluster]['logLikelihood'],
-                  'weight': final_result[best_cluster]['weight'],
-                  'time': time.time() - start_time,
-                  'logLikelihood_list': final_result[best_cluster]['logLikelihood']
-                  }
-
-        return output
+        return result
     
